c137126e9af168b3e2e5dd74c332672008dfb18c
aliramirez08 <aliramirez08@aol.com>
2025-10-26 18:12:33 -0400

    update hiltTest

--- app/src/androidTest/java/com/example/unitconverter/HiltIntegrationTest.kt
+++ app/src/androidTest/java/com/example/unitconverter/HiltIntegrationTest.kt
@@ -1,24 +1,29 @@
 package com.example.unitconverter
 
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextInput
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 @HiltAndroidTest
 class HiltIntegrationTest {
 
-    @get:Rule(order = 0) val hiltRule = HiltAndroidRule(this)
-    @get:Rule(order = 1) val compose = createAndroidComposeRule<MainActivity>()
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val compose = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
 
     @Test
     fun e2e_temperature_returns_999() {
-        hiltRule.inject()
         compose.onNodeWithText("Category").performClick()
         compose.onNodeWithText("Temperature").performClick()
         compose.onNodeWithText("Enter Value").performTextInput("100")
